4149350850ce3c5f12f256d1659a0d3f6ed77683
Christian Lisangola <jochri3@gmail.com>
2021-08-02 20:31:12 +0100

    populate db with fake data

--- app.js
+++ app.js
@@ -8,9 +8,16 @@ const app = express();
 app.use(express.static(path.join(__dirname, "/frontend/build")));
 
 const contacts = [
-  { id: 1, name: "John", age: 25 },
-  { id: 2, name: "Jane", age: 30 },
-  { id: 3, name: "Bob", age: 40 },
+  { id: 1, name: "Jean-Cédrick Chapelier", age: 25 },
+  { id: 2, name: "Jamilla Sam", age: 30 },
+  { id: 3, name: "Jule Césare", age: 40 },
+  { id: 4, name: "Alain Kaluitu Kadioko", age: 35 },
+  { id: 5, name: "Mbala Mbuta Biscot", age: 30 },
+  { id: 6, name: "Matumona Roum", age: 35 },
+  { id: 7, name: "Mbayo Kibemba", age: 30 },
+  { id: 8, name: "Felix Le Gouverneur", age: 35 },
+  { id: 9, name: "Samy Kifaru", age: 30 },
+  { id: 10, name: "Drozophilia Melanogaster", age: 35 },
 ];
 
 app.get("/api/contacts", (req, res) => {
