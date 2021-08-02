ae9e76b49d9a191a3a09cd820eed6bd4ac33255e
Christian Lisangola <jochri3@gmail.com>
2021-08-02 20:09:50 +0100

    resolve __dirname for ES6 modules

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import express from "express";
 import path from "path";
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.static(path.join(__dirname, "/frontend/build")));
